3e4f00e438391c7ef143075b8c4dee8a114ffea2
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-30 21:41:15 -0700

    s

--- ExecutionerV4-main-main/act3/scene5/obstaclecouse.gd
+++ ExecutionerV4-main-main/act3/scene5/obstaclecouse.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	FactorySound.playsound()
+	CitySound.playsound()
 	gameSaver.save_game("res://Act3/scene5/obstaclecourse.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- ExecutionerV4-main-main/act3/scene5/obstaclecouse3.gd
+++ ExecutionerV4-main-main/act3/scene5/obstaclecouse3.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	FactorySound.playsound()
+	CitySound.playsound()
 	gameSaver.save_game("res://Act3/scene5/obstaclecourse3.tscn")
 
 
