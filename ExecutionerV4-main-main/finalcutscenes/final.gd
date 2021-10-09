b1192983bc6e9fc0b1cdf3e61efc46a8dc31d3be
Serena Zhou <mediocrephilosophie@gmail.com>
2021-10-09 11:57:03 -0700

    m

--- ExecutionerV4-main-main/finalcutscenes/final.gd
+++ ExecutionerV4-main-main/finalcutscenes/final.gd
@@ -8,13 +8,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
+	yield(get_tree().create_timer(92), "timeout")
+	$RichTextLabel/AnimationPlayer.play("New Anim")
+	$RichTextLabel2/AnimationPlayer.play("New Anim")
+	yield(get_tree().create_timer(10), "timeout")
+	$RichTextLabel3/AnimationPlayer.play("New Anim")
+	yield(get_tree().create_timer(7), "timeout")
+	Transition.change_scene("res://global/menu/menu.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+	#if VideoPlayer.finished
 
 
-func _on_Timer_timeout():
-	$AudioStreamPlayer.pl
+#func _on_Timer_timeout():
+	#$AudioStreamPlayer.play()
