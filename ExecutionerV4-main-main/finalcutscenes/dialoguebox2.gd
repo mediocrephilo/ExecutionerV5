196e7a30b1f258190551f3884232b3042e5da3de
Serena Zhou <mediocrephilosophie@gmail.com>
2021-10-11 22:17:59 -0700

    s

---
+++ ExecutionerV4-main-main/finalcutscenes/dialoguebox2.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	pass
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_up"):
+		$AudioStreamPlayer.play()
+		Transition.change_scene("res://finalcutscenes/final.tscn")
+	if Input.is_action_just_pressed("ui_down"):
+		Transition.change_scene("res://finalcutscenes/final2.tscn")
