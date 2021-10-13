4f4eb58e3e04a8b525e01ee1a16888f908d5b3ea
Serena Zhou <mediocrephilosophie@gmail.com>
2021-10-12 18:07:01 -0700

    m

--- ExecutionerV4-main-main/global/menu/credits.gd
+++ ExecutionerV4-main-main/global/menu/credits.gd
@@ -9,35 +9,40 @@ var page = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.hide()
+	$AnimatedSprite2.hide()
 	$People.show()
 	$SoundsLinks.hide()
 	$SoundsLinks2.hide()
-	yield(get_tree().create_timer(3), "timeout")
+	$Label.hide()
 	$AnimatedSprite.show()
-	
+	yield(get_tree().create_timer(2), "timeout")
+	$Label.show()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if page == 0:
+		$People.show()
+		$SoundsLinks.hide()
+		$SoundsLinks2.hide()
+		$AnimatedSprite2.hide()
+		$AnimatedSprite.show()
+	if page == 1:
+		$People.hide()
+		$SoundsLinks.show()
+		$SoundsLinks2.hide()
+		$AnimatedSprite2.show()
+		$AnimatedSprite.show()
+	if page == 2:
+		$People.hide()
+		$SoundsLinks.hide()
+		$SoundsLinks2.show()
+		$AnimatedSprite2.show()
+		$AnimatedSprite.hide()
+			
+	if Input.is_action_just_pressed("ui_cancel"):
+		Transition.change_scene("res://global/menu/menu.tscn")
 	if Input.is_action_just_pressed("ui_right"):
-		if page == 0:
-			$People.hide()
-			$SoundsLinks2.hide()
-			$SoundsLinks.show()
-			yield(get_tree().create_timer(3), "timeout")
-			$AnimatedSprite2.show()
-			page += 1
-		if page == 1:
-			$People.hide()
-			$SoundsLinks.hide()
-			$SoundsLinks2.show()
+		if page < 2:
 			page += 1
 	if Input.is_action_just_pressed("ui_left"):
-		if page == 2:
-			$SoundsLinks2.hide()
-			$SoundsLinks.show()
-			$People.hide()
-			page -= 1
-		if page == 1:
-			$SoundsLinks2.hide()
-			$SoundsLinks.hide()
-			$People.show()
+		if page > 0:
 			page -= 1
