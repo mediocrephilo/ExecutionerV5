0b730f7e8acfc3ef2db9a84a538aecc8a6541d46
Serena Zhou <mediocrephilosophie@gmail.com>
2021-10-16 20:45:43 -0700

    d

--- ExecutionerV4-main-main/Act1/playerscenes/scene5/Area2D.gd
+++ ExecutionerV4-main-main/Act1/playerscenes/scene5/Area2D.gd
@@ -5,7 +5,7 @@ extends Area2D
 
 func _on_Area2D_body_entered(body):
 	if body.name == "player":
+		$AudioStreamPlayer.play()
 		GameOver.transition("res://Act1/playerscenes/scene5/scene5.tscn")
-		$AudioStreamPlayer.playsound()
 		GameOver.change(true)
 		Transition.change_scene("res://global/gameover/gameOver.tscn")
